cd4b157b648d8eb10cb67baffd8cfeb85e71a6e9
Lisxt <alice14072005@gmail.com>
2025-01-15 15:11:02 -0300

    Create DuplamenteCircular.h

---
+++ DuplamenteCircular.h
@@ -0,0 +1,17 @@
+
+struct lista {
+    int codAluno;
+    int nota;
+    struct lista* prox;
+    struct lista* ant;
+};
+typedef struct lista Lista;
+
+Lista* inicializa (void);
+Lista* insere_inicio (Lista* l, int cod, int nota);
+void imprime (Lista* l);
+int vazia (Lista* l);
+Lista* busca (Lista* l, int cod);
+//Lista* retira (Lista* l, int cod);
+//void libera (Lista* l);
+Lista* insere_ordenado (Lista* l, int cod, int nota);
